20ba786f112260817bce7464f0e9690929860006
Franklin <fran.developer.2022@gmail.com>
2023-09-06 17:59:45 -0600

    Implementacion Utils para Envio Correos.

---
+++ MiParcial/Controllers/PruebasController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiParcial.Utils;
+
+namespace MiParcial.Controllers
+{
+    //Este es un controller utilizado unicamente para realizar pruebas.
+    public class PruebasController : Controller
+    {
+        public IActionResult Index()
+        {            
+            return View();
+        }
+
+        public IActionResult EnviarCorreo()
+        {
+            string correoDestinatario = "frankexe2019@gmail.com";
+            string asuntoCorreo = "prueba";
+            string cuerpoCorreo = "pruebaaa";
+
+            bool enviadoExitosamente = GmailSender.SendEmail(correoDestinatario, asuntoCorreo, cuerpoCorreo);
+            if (enviadoExitosamente)
+            {
+                TempData["SuccessMessage"] = "Correo enviado con éxito.";
+            }
+            else
+            {
+                //ERROR
+                ModelState.AddModelError(string.Empty, "Error al enviar correo.");
+            }
+
+
+            return RedirectToAction("Resultado");
+        }
+
+        public IActionResult Resultado()
+        {
+            // Enviar mensajes para renderizar en la vista
+            if (TempData.ContainsKey("SuccessMessage"))
+            {
+                ViewBag.SuccessMessage = TempData["SuccessMessage"];
+            }
+
+            return View();
+        }
+    }
+}
---
+++ MiParcial/Utils/GmailSender.cs
@@ -0,0 +1,55 @@
+﻿using System.Net.Mail;
+using System.Net;
+using System.Text;
+
+namespace MiParcial.Utils
+{
+    public static class GmailSender
+    {
+        private static string correoEmisor = "+";
+        private static string appEmailPassword = "+";
+
+        /// <summary>
+        /// Metodo para Enviar un Correo Electronico
+        /// </summary>
+        /// <param name="correoDestinatario"> Email de Destino a enviar.</param>
+        /// <param name="asunto"> Asunto o Encabezado del Correo</param>
+        /// <param name="cuerpoMensaje"> Mensaje que se desea enviar por Email.</param>        /// 
+        public static bool SendEmail(string correoDestinatario, string asunto, string cuerpoMensaje)
+        {
+            string from = correoEmisor;
+            string to = correoDestinatario;
+            string subject = asunto;
+            string body = cuerpoMensaje;
+
+            using (MailMessage mail = new MailMessage(from, to))
+            {
+                mail.Subject = subject;
+                mail.Body = body;
+                mail.IsBodyHtml = true; // Puedes establecer esto a true si deseas enviar un correo HTML
+
+                using (SmtpClient client = new SmtpClient("smtp.gmail.com"))
+                {
+                    
+                    client.Port = 587;
+                    client.Credentials = new NetworkCredential(correoEmisor, appEmailPassword);
+                    client.EnableSsl = true;
+
+                    try
+                    {
+                        client.Send(mail);
+                        Console.WriteLine("Correo enviado con éxito");
+                        return true; //enviado con exito.
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error al enviar el correo: {ex.Message}");
+                        return false; //error al enviar.
+                    }
+                }
+            }
+        }
+
+    }
+}
